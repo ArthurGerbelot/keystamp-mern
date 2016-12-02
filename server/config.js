4604bf1e78dbb24ff617b5e53a9d42d4938bcdc2
Arthur G <arthur.gerbelot@gmail.com>
2016-12-01 19:21:17 -0500

    Test for Heroku db

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://heroku_1h1v2hfz:evr0jvr0r27hjnppi0rk4hci98@ds119748.mlab.com:19748/heroku_1h1v2hfz',
   port: process.env.PORT || 8000,
 };
 
